cc99f44571590fe191ff6c17e857991b5a4cd6c1
Alin Hîrtopanu <alinhirtopanu@gmail.com>
2022-02-24 18:04:06 +0200

    compulsory

---
+++ lab1/compulsory/src/compulsory/Exercitii.java
@@ -0,0 +1,29 @@
+package compulsory;
+
+public class Exercitii {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+		String[] languages = {"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
+		
+		int n = (int) (Math.random() * 1_000_000);	
+		n *= 3;
+		n += Integer.parseInt("10101", 2);
+		n += Integer.parseInt("FF", 16);
+		n *= 6;
+		
+		int result = n, copy;
+		while (result > 9) {
+			copy = result;
+			result = 0;
+			
+			while (copy != 0) {
+				result += copy % 10;
+				copy /= 10;
+			}
+		}
+		
+		System.out.println(languages[result]);
+	}
+}
