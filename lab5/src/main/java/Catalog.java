bed2b563bd76f57a36aa065a0a31696054129dc2
Alin Hîrtopanu <alinhirtopanu@gmail.com>
2022-03-31 15:37:13 +0300

    very good very nice

--- lab5/src/main/java/Catalog.java
+++ lab5/src/main/java/Catalog.java
@@ -1,3 +1,4 @@
+import java.io.Serializable;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -6,10 +7,32 @@ public class Catalog implements Serializable {
     private String name;
     private List<Item> items;
 
+    public Catalog() {}
+
     public Catalog(String name) {
 
         this.name = name;
         this.items = new ArrayList<>();
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public void setItems(List<Item> items) {
+
+        this.items = items;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public List<Item> getItems() {
+
+        return items;
     }
 
     public void add(Item item) {
--- lab5/src/main/java/Item.java
+++ lab5/src/main/java/Item.java
@@ -1,3 +1,4 @@
+import java.io.Serializable;
 import java.util.Map;
 import java.util.HashMap;
 
